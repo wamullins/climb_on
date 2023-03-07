8ddb28d9e535e9d2f096e50525646dfdb54764d5
Andrew Mullins <mullinsa428@gmail.com>
2023-03-06 19:40:55 -0500

    Updated Sections

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
+reports=[]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    rendered_reports=""
+    for report in reports:
+        rendered_reports+=f"""<div>{report["difficulty"]}: {report["attempts"]}: {report["date"]}: {report["notes"]}</div>"""
+    return """<div>
+    <h1>Climb On!</h1>
+    <form method="post">
+        <div>
+            <label>Difficulty</label>
+            <input name="difficulty"/>
+        </div>
+        <div>
+            <label>Number of Attempts</label>
+            <input named="attempts"</label>
+        </div>
+        <div>
+            <label>Date Completed</label>
+            <input named="date"</label>
+        </div>
+        <div>
+            <label>Notes</label>
+            <textarea name="notes"></textarea>
+        </div>
+        <button>Send it</button>
+    </form>"""+rendered_reports+"""</div>"""
 
-    
+@app.route("/",methods=["POST"])
+def handle_submission():
+    for key in request.form.keys():
+        print(request.form.get(key))
+    reports.append(request.form.to_dict())
+    return hello_world()
