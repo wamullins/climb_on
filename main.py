50109f45cff5a0e7f82e024cab2f49c41f83567d
Andrew Mullins <mullinsa428@gmail.com>
2023-03-06 21:22:36 -0500

    split html code over to index. Added style.css

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
@@ -6,30 +6,7 @@ reports=[]
 
 @app.route("/")
 def hello_world():
-    rendered_reports=""
-    for report in reports:
-        rendered_reports+=f"""<div>{report["difficulty"]}: {report["attempts"]}: {report["date"]}: {report["notes"]}</div>"""
-    return """<div>
-    <h1>Climb On!</h1>
-    <form method="post">
-        <div>
-            <label>Difficulty</label>
-            <input name="difficulty"/>
-        </div>
-        <div>
-            <label>Number of Attempts</label>
-            <input name="attempts"/>
-        </div>
-        <div>
-            <label>Date Completed</label>
-            <input name="date"/>
-        </div>
-        <div>
-            <label>Notes</label>
-            <textarea name="notes"></textarea>
-        </div>
-        <button>Send it</button>
-    </form>"""+rendered_reports+"""</div>"""
+    return render_template("index.html", reports=reports)
 
 @app.route("/",methods=["POST"])
 def handle_submission():
